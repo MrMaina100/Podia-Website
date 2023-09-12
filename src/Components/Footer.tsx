9e75cc553084afa48d80bdbab33cdc3eddc98778
Derrick Maina <mburumaina648@gmail.com>
2023-09-12 11:34:33 +0300

    updates on the footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 import {AiFillGithub} from 'react-icons/ai'
 import {FaXTwitter} from 'react-icons/fa6'
 import {AiFillLinkedin} from 'react-icons/ai'
-
+import {SiUpwork} from 'react-icons/si'
 const Footer = () => {
   return (
     <div className="container mx-auto max-w-[1400px] pt-20 px-6 md:px-0">
@@ -56,15 +56,32 @@ const Footer = () => {
          <div className='pb-8 '>
             <hr />
             <div className="flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-5 justify-between items-center pt-2">
-                <p>Designed by Derrick Maina</p>
+                <p className='hover:text-purple-200'>
+                  Designed by Derrick Maina
+                  
+                  </p>
 
                 <div className="flex flex-row space-x-3">
-                 <a href="">
+                 <a href="https://github.com/MrMaina100"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  title="Open in new tab">
                   <AiFillGithub/>
                   </a> 
                   <a href=""> <FaXTwitter/> </a>
 
-                  <a href=""> <AiFillLinkedin/> </a>
+                  <a href="https://www.linkedin.com/in/derrick-maina-b20752280/"
+                   target="_blank"
+                  rel="noreferrer noopener"
+                  title="Open in new tab"> <AiFillLinkedin/> </a>
+                  <a 
+                  href="https://www.upwork.com/freelancers/~01a9097cbc57a005c1"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  title="Open in new tab"
+                  >
+                     <SiUpwork/>
+                  </a>
                 </div>
                 
             </div>
